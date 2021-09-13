667948eb98967063f699b1e1dc5ade4854c29bb9
Abhinav Pandey <abhinavp650@gmail.com>
2021-09-13 16:17:38 +0530

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace OLCalc
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Button CalBtn = ((Button)sender);
+            textBox1.Text = CalBtn.Text != "=" ? textBox1.Text + CalBtn.Text : dt.Compute(textBox1.Text, "").ToString();
+        }
+    }
+}
